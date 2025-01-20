7d372dfc1d7aec6deb78d87e9c7f6ad5d92cdc19
1y33 <catalin.milu.2004@gmail.com>
2025-01-20 14:09:11 +0200

    Day5

--- day4/layerNorm.cu
+++ day4/layerNorm.cu
@@ -63,12 +63,12 @@ __global__ void layerNorm(Tensor* A) {
 int main() {
     const int rows = 1;
     const int cols = 3;
+    const int BLOCKSIZE = 16;
     const int tensorSize = rows * cols;
     const int size = rows * cols * sizeof(float);
-    dim3 threadsPerBlock(16*16);
-    dim3 numBlocks((rows + threadsPerBlock.x - 1) / threadsPerBlock.x);
+    dim3 blockDim(16*16);
+    dim3 gridDim((rows + BLOCKSIZE - 1) / BLOCKSIZE);
     size_t sharedMemorySize = 3 * rows * sizeof(float);
-
     // Shared memory for mean, variance, invstdev
     // we need 3 different arrays of rows * size(float)
 
@@ -77,29 +77,34 @@ int main() {
         5.0f, 1.5f, 2.0f,
     };
 
-    // now we allocate the data on the device and copy it 
-    float* d_data;
-    cudaMalloc(&d_data, size);
-    cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice);
-    
+
     // now lets allocate the tensor on the device
-    // we create our tensor but we use teh device data as the data
+    // we create our tensor but we use the device data as the data
     // and we only copy the block
+    
+    float* d_data; // pointer to device data
+    cudaMalloc(&d_data, size);   // alocate the space
+    cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice); // copy the h_data to the d_data on the gpu
+     
+    Tensor h_tensor; // now we create the host tenosr on gpu
+    h_tensor.data = d_data; // the pointer to the GPU !
+    h_tensor.rows = rows; // copy rows
+    h_tensor.cols = cols; // copy cols
 
-    Tensor h_tensor;
-    h_tensor.data = d_data;
-    h_tensor.rows = rows;
-    h_tensor.cols = cols;
 
-    Tensor* d_tensor;
-    cudaMalloc(&d_tensor, sizeof(Tensor));
+    Tensor* d_tensor; // now create a device tensor
+    cudaMalloc(&d_tensor, sizeof(Tensor)); // we allocate the space
     cudaMemcpy(d_tensor, &h_tensor, sizeof(Tensor), cudaMemcpyHostToDevice);
+    // now we copy on the d_tensor from the h_tenosr
 
 
-    layerNorm<<<numBlocks, threadsPerBlock, sharedMemorySize>>>(d_tensor);
-    cudaDeviceSynchronize();
+    layerNorm<<<gridDim ,blockDim, sharedMemorySize>>>(d_tensor);
+    // we launch our kernel now 
+
+    cudaDeviceSynchronize(); // syncing
     cudaMemcpy(h_data, d_data,size, cudaMemcpyDeviceToHost);
-
+    // now copy back the struct
+    
     std::cout << "Normalized Matrix:" << std::endl;
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
