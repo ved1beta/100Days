5a6467f287894ed06192ecbc783afa4a5941a4d2
1y33 <catalin.milu.2004@gmail.com>
2025-01-21 13:26:24 +0200

    typos

--- day6/AdditionKernel/pythontest.py
+++ day6/AdditionKernel/pythontest.py
@@ -1,11 +1,6 @@
 import torch
-import additionkernel  # Ensure the module is built and installed
+import additionkernel  
 
-# Create a tensor on the GPU with int32 type
 input_tensor = torch.tensor([1, 2, 3, 4, 5], dtype=torch.int32).cuda()
-
-# Call the addition function
 additionkernel.addition(input_tensor, input_tensor.size(0))
-
-# Print the result
 print("Result after addition:", input_tensor)
